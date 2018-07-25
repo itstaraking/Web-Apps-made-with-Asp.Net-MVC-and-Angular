5227e25c403988dff93a4b47e9dd8906f3f850ee
itstaraking <fairgameapps@gmail.com>
2018-07-24 23:09:42 -0400

    Add files via upload

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartIT.Employee.MockDB;
+
+namespace ToDoAngular.Ui.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        private readonly ITodoRepository _todoRepository;
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+        // GET: api/Todo  
+        [Route("~/api/GetAllTodos")]
+        [HttpGet]
+        public IEnumerable<Todo> GetAllTodos()
+        {
+            return _todoRepository.GetAll();
+        }
+
+        [Route("~/api/AddTodo")]
+        [HttpPost]
+        public Todo AddTodo([FromBody]Todo item)
+        {
+            return _todoRepository.Add(item);
+        }
+
+        [Route("~/api/UpdateTodo")]
+        [HttpPut]
+        public Todo UpdateTodo([FromBody]Todo item)
+        {
+            return _todoRepository.Update(item);
+        }
+
+        [Route("~/api/DeleteTodo/{id}")]
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var findTodo = _todoRepository.FindById(id);
+            if (findTodo != null)
+                _todoRepository.Delete(findTodo);
+        }
+    }
+}
